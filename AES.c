c088ea80cc0aa30e5d9b94fc13a82aab4138e47d
Davidsvivo <davidsvivobook69@gmail.com>
2024-08-16 01:28:13 +0530

    AES.c

---
+++ AES.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/aes.h>
+// AES key for encryption and decryption (128-bit key)
+unsigned char aes_key[16];
+// Function to encrypt plaintext using AES
+void aes_encrypt(unsigned char *plaintext, unsigned char *ciphertext) {
+    AES_KEY key;
+    AES_set_encrypt_key(aes_key, 128, &key);
+    AES_encrypt(plaintext, ciphertext, &key);
+}
+// Function to decrypt ciphertext using AES
+void aes_decrypt(unsigned char *ciphertext, unsigned char *decryptedtext) {
+    AES_KEY key;
+    AES_set_decrypt_key(aes_key, 128, &key);
+    AES_decrypt(ciphertext, decryptedtext, &key);
+}
+int main() {
+    unsigned char plaintext[100];
+    unsigned char ciphertext[AES_BLOCK_SIZE]; // AES block size is 128 bits
+    unsigned char decryptedtext[AES_BLOCK_SIZE];
+    printf("enter text: ");
+    fgets(plaintext, sizeof(plaintext), stdin);
+    plaintext[strcspn(plaintext, "\n")] = '\0';    // Encrypt the plaintext
+    
+    printf("enter key: ");
+    fgets(aes_key, 16, stdin);
+    aes_encrypt(plaintext, ciphertext);
+
+    // Decrypt the ciphertext
+    aes_decrypt(ciphertext, decryptedtext);
+    printf("Original message: %s\n", plaintext);
+    printf("Encrypted message: ");
+    for (int i = 0; i < AES_BLOCK_SIZE; i++) {
+        printf("%02x", ciphertext[i]);
+    }
+    printf("\n");
+    printf("Decrypted message: %s\n", decryptedtext);
+    return 0;
+}
+//gcc  aes.c -lssl -lcrypto
